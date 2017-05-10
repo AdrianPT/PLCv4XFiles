13cf994fdcdf8a95c16b320927fe898fd41a7b3a
Adrian Ferreira <correoadrianf@hotmail.com>
2017-05-10 21:07:25 +0100

    Funcionando simplex, con libreria OPC DLL incluida

---
+++ PLCv4XFiles/Controllers/DriversController.cs
@@ -0,0 +1,51 @@
+﻿using PLCv4XFiles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PLCv4XFiles.Controllers
+{
+    public class DriversController : ApiController
+
+    {
+
+
+       Driver[] Drivers = new Driver[]
+       {
+            new Driver { Id = 1, tagValor = "Tag.C0", tagSerie = "Tag.C1"},
+            new Driver { Id = 2, tagValor = "Tag.C0", tagSerie = "Tag.C1"},
+            new Driver { Id = 3, tagValor = "Tag.C0", tagSerie = "Tag.C1"},
+       };
+
+        public IEnumerable<Driver> GetAllDrivers()
+        {
+            return Drivers;
+        }
+
+
+        public IHttpActionResult GetDriver(int id)
+        {
+            var DriverAux = Drivers.FirstOrDefault((p) => p.Id == id);
+
+            if (DriverAux == null)
+
+            {
+
+                return NotFound();
+
+            }
+
+
+            return Ok(DriverAux);
+        }
+
+
+
+
+
+
+    }
+}
---
+++ PLCv4XFiles/Models/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PLCv4XFiles.Models
+{
+    public class Driver
+    {
+
+        public int Id { get; set; }
+        public string tagValor { get; set; }
+        public string tagSerie { get; set; }
+
+        public int valor { get; set; }
+        public int serie { get; set; }
+
+    }
+}

2d7088a5973680e1b6f3365bd56af5509ef440f1
AdrianFerreira <AdrianFerreira@PC2-ADRIAN>
2017-05-12 17:29:02 +0100

    Ahora agregue trhead sin nada realmente salvoue cada segundo esta "haciendo"

--- PLCv4XFiles/Global.asax.cs
+++ PLCv4XFiles/Global.asax.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
@@ -12,6 +15,61 @@ namespace PLCv4XFiles
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            iniciarTask();
         }
+
+
+        protected void iniciarTask()
+        {
+
+            try
+            {
+
+                IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+                scheduler.Start();
+
+
+                IJobDetail job = JobBuilder.Create<DBWriter>()
+                    .WithIdentity("UniqueWork", "Group")
+                    .Build();
+
+
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("Initio", "Group")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(1)
+                        .WithRepeatCount(0)
+                        .RepeatForever()
+                     )
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                scheduler.ScheduleJob(job, trigger);
+
+                // A los 10 segundos duerme
+                Thread.Sleep(TimeSpan.FromSeconds(400));
+
+                // apaga el scheduler cuando estoy listo para cerrar el programa
+                scheduler.Shutdown();
+
+            }
+            catch (SchedulerException se)
+            {
+
+                // SimpleLog.Log(se);
+
+            }
+
+
+
+        }
+
+
+
+
+
+
     }
 }
